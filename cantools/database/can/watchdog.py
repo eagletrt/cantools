2c974e74216b34454b7e7783979f495f9fe0a05f
EDB02 <edalbianco02@gmail.com>
2023-06-21 18:09:19 +0200

    added interval from id

--- cantools/database/can/watchdog.py
+++ cantools/database/can/watchdog.py
@@ -21,14 +21,33 @@ extern "C" {{
 #endif // primary_NETWORK_H
 '''
 
+INTERVAL_FROM_ID = '''
+static int primary_watchdog_interval_from_id(uint16_t message_id) {{
+    switch (message_id) {{
+{messages}
+    }}
+    return -1;
+}}'''
+
+def _interval_name(database_name, msg_name):
+    return f'{database_name}_INTERVAL_{msg_name}'
+
 def _generate_intervals(messages, database_name):
     ret = ''
     for msg in messages:
         if msg.cycle_time is not None:
-            ret += f'#define {database_name}_INTERVAL_{msg.name.upper()} {msg.cycle_time}\n'
+            ret += f'#define {_interval_name(database_name, msg.name.upper())} {msg.cycle_time}\n'
     return ret
+
+def _generate_intervals_from_id(messages, database_name):
+    tmp = ''
+    for msg in messages:
+        if msg.cycle_time is not None:
+            tmp += f'       case {msg.frame_id}: return {_interval_name(database_name, msg.name.upper())};\n'
+    return INTERVAL_FROM_ID.format(messages=tmp)
 
 def generate_watchdog(database, database_name):
     messages = database.messages
-    return WATCHDOG.format(body=_generate_intervals(messages, database_name))
+    body = _generate_intervals(messages, database_name) + _generate_intervals_from_id(messages, database_name)
+    return WATCHDOG.format(body=body)
     
